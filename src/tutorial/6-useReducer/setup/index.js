ad57563a71bbe90361078915b1da3107c479061d
jasem66 <jasemsarafraz66@gmail.com>
2023-01-26 17:40:09 +0200

    change

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,38 +1,88 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
+import { useEffect } from 'react'
 // reducer function
 
 const Index = () => {
-const reducer =(state,action)=>{
-
-}
-  const defaultState ={
-    people : [data],
-    name : false,
-  }
-
   const [name, setName] = useState('')
-  const [state, dispatch] = useReducer(reducer,defaultState)
+  const [list, setlist] = useState([])
 
-  const handleSubmit =(e)=>{
-e.preventDefault()
+  const randomId = new Date().getTime().toString()
+
+  const reducer = (state, action) => {
+    if (action.type === 'ADD') {
+      const newPeople = [...state.people, action.payload]
+      return {
+        ...state,
+        people: newPeople,
+      }
+    }
+    if (action.type === 'MODAL_OPEN') {
+      return {
+        ...state,
+        modalOpen: true,
+        modalContent: 'enter it',
+      }
+    }
+
+    throw new Error('no matching action type')
   }
+  const defaultState = {
+    people: [list],
+    name: '',
+    modalOpen: false,
+    modalContent: '',
+  }
+
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      const person = { name, id: randomId }
+      dispatch({
+        type: 'ADD',
+        payload: person,
+      })
+      setName('')
+    }
+    if (!name) {
+      dispatch({ type: 'MODAL_OPEN' })
+         setName('')
+    } else {
+      setName('')
+    }
+  }
+  // useEffect(() => {
+  //  const timeOut=setTimeout(() => {
+
+  //  }, 2000);
+  //  return()=>clearTimeout(timeOut)
+  // })
 
   return (
     <div>
-      <form action="">
-        <input type="text" 
-        onChange={(e)=>setName(e.target.value)}
-        value={name} />
-        <button>Add</button>
+      {(state.modalOpen  ) && (
+        <div className='w-50 h-100 bg-success p-5 m-5 rounded'>
+          {state.modalContent}
+        </div>
+      )}
+      <form className='p-5' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          name='name'
+        />
+        <button className='btn btn-primary text-danger'>Add</button>
       </form>
-      <div>{
-        data.map((item)=>{
-         return <p>{item.name}</p>
-        })
-      }</div>
+      <div>
+        {state.people.map((item) => {
+          return <p key={item.id}>{item.name}</p>
+        })}
+      </div>
     </div>
   )
-  }
+}
 export default Index
